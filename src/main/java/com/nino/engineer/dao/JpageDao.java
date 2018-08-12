b1f6245dd3584d2cd91dd24e383382033dbfc4eb
DaiYuanchuan <1224073217@qq.com>
2018-08-12 23:26:35 +0800

    新增添加项目接口

--- src/main/java/com/nino/engineer/dao/JpageDao.java
+++ src/main/java/com/nino/engineer/dao/JpageDao.java
@@ -1,6 +1,6 @@
 package com.nino.engineer.dao;
 
-import com.nino.engineer.domain.User;
+import com.nino.engineer.domain.*;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
@@ -14,4 +14,16 @@ public interface JpageDao {
     User isExistenceUserID(int id);
     int register(User user);
     List<User> lookupAllUsers();
+    /* 匹配用户权限 */
+    Integer Matching_user_rights(@Param("u_id") int u_id,int j_id);
+    /* 是否存在项目名 */
+    List<Project> IsRepeat(String name);
+    /* 添加地址信息 */
+    int Add_address_info(Location location);
+    /* 添加项目数据 */
+    int Create_Project (Project project);
+    /* 添加项目状态 */
+    int AddProjectStatusInfo(Project_status project_status);
+    /* 添加权限信息 */
+    int Add_project_permission_information (Permissions_detailed permissions_detailed);
 }
